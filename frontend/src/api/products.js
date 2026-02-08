37b632b52e67b8422307d9a23f17f7689412d8e3
rahul shetye <rahulshetye@rahuls-MacBook-Air.local>
2026-02-08 17:04:19 +0530

    Add frontend route

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,3 +1,5 @@
+let REACT_APP_API_URL = process.env.REACT_APP_API_URL || "https://naksh-jewels-production-5dfc.up.railway.app";
+
 export const fetchProducts = async () => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}/products`)
 ;
